095e8485d372a50d45900fe8e05a663182142351
RoberSmith <>
2021-05-07 18:50:17 +0000

    fixed

--- script.js
+++ script.js
@@ -1,20 +1,18 @@
 document.getElementById('button').addEventListener
-('click',randomizer)
+('click' ,randomizer)
 let randomNumber = 0
 let userGuess = 0
-//varables for 
+//  varables for numbers
 
 function randomizer () {
-  userGuess = document.getElementById('input')
-  .value
+  userGuess = document.getElementById('input').value
   userGuess = parseInt(userGuess)
 
-  //Our number generator from 1-6
-  randomNumber = (Math.random() * 6)+ 1
+  //  Our number generator from 1-6
+  randomNumber = (Math.random() * 6) + 1
   randomNumber = parseInt(randomNumber)
 
-  if (userGuess == randomNumber) {
+  if (userGuess === randomNumber) {
    document.getElementById('answer').innerHTML =
-   "you win"
-  }
+    "you win"
 }
