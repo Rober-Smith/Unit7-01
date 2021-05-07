b93e141a36d5f668f8c1e49f5d0669bd05dd26fb
RoberSmith <>
2021-05-07 17:01:30 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,20 @@
+document.getElementById('button').addEventListener
+('click',randomizer)
+let randomNumber = 0
+let userGuess = 0
+//varables for 
+
+function randomizer () {
+  userGuess = document.getElementById('input')
+  .value
+  userGuess = parseInt(userGuess)
+
+  //Our number generator from 1-6
+  randomNumber = (Math.random() * 6)+ 1
+  randomNumber = parseInt(randomNumber)
+
+  if (userGuess == randomNumber) {
+   document.getElementById('answer').innerHTML =
+   "you win"
+  }
+}
